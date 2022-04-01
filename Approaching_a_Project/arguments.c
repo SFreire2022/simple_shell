c1959fcf1ee666322a90dcb467f5961adaa1335d
SFreire2022 <sebastiancazenave@gmail.com>
2022-04-01 10:34:39 -0300

    Fix betty style

--- Approaching_a_Project/arguments.c
+++ Approaching_a_Project/arguments.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * main - Main program to get args from comand line
+ * @ac: argument counter.
+ * @av: argument vector (strings array).
+ * Return: if ok return 0.
+ */
 int main(int ac, char **av)
 {
- 	int i=0;
+	int i = 0;
 
- 	/* First argument is executable name only */
- 	printf("exe name=%s\n", av[0]);
+	/* First argument is executable name only */
+	printf("exe name=%s\n", av[0]);
 
-	for (i=1; av[i] != NULL; i++)
+	for (i = 1; av[i] != NULL; i++)
 		printf("arg%d=%s\n", i, av[i]);
 
- 	return (0);
+		return (0);
 }
