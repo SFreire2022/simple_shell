8bc49ec74079f286090a3b5a56978c3a934b5ace
MartinDeFrancesco <defrancescoma@gmail.com>
2022-04-04 07:53:39 -0700

    strtok without using strtok

---
+++ Approaching_a_Project/strtoknt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<string.h>
+/**
+ * main - strtok without using strtok
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char str[43] = "This is the strok function without strtok";
+	char chr[] = " ";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == chr[0])
+			str[i] = '\0';
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (str[j] != '\0')
+			printf("%c", str[j]);
+		else
+			printf("\n");
+	}
+	printf("\n");
+	return (0);
+}
