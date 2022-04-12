22905859386e9e55c1f2243943fde6430c7361ad
MartinDeFrancesco <defrancescoma@gmail.com>
2022-04-11 19:34:16 -0700

    main/.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#define TRUE (1 == 1)
+
+void check_mode(ssize_t nread, char **clon_av, char *line);
+void sig_trap(int sig);
+void print_prompt(void);
+char **input_tokenizer(char *line, ssize_t nread, const char *sep);
+char *chk_fork_execve(char **clon_av, char **envp);
+char *chk_command(char *command);
+void chk_builtin(char **clon_av, char *line, char **envp);
+
+#endif
